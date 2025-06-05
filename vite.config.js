f0771ca6b168270fc0698f992fc164f3d46ea703
DynamicMonki <vivek.s.r.dev@gmail.com>
2025-06-05 20:10:44 +0530

    Fix of the vite.config.js file

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
-import ghPages from 'vite-plugin-gh-pages'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte(), ghPages()],
+  plugins: [svelte()],
   base: 'https://github.com/DynamiteMonki/dog_image_viewer.git'
 })
