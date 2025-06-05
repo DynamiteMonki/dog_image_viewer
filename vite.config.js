7c484a968f5dd19a4dddc5b127db2815c9857850
DynamicMonki <vivek.s.r.dev@gmail.com>
2025-06-05 20:08:45 +0530

    Fixed the vite.config.js file and sorta trying to make this work

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import ghPages from 'vite-plugin-gh-pages'
 
 // https://vite.dev/config/
 export default defineConfig({
